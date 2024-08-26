d6bb8d8beac724a05ff023b8f82a2eb1d78ca08b
Adhanio <adhaniwahab@gmail.com>
2024-08-26 14:56:24 +0800

    files upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
+
+function openVideo() {
+var videoUrl = "./adhaniportfolioasset/1.PALE - backup_final - Android - Unity 2019.4.31f1_ _DX11_ 2023-07-04 15-58-52.mp4"; // Replace with your video file URL
+window.open(videoUrl, "_blank"); // Open the video in a new tab
+}
+
+
+                
+
